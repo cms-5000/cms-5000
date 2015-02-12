a08919d85917bc044da1fb5d29f9162fc95e3138
Sebastian Herrmann <sebastian@herrherrmann.net>
2015-02-12 13:15:36 +0100

    Bam. Some updates for the page feature.

--- js/app.js
+++ js/app.js
@@ -1,48 +1,49 @@
 App = Ember.Application.create({});
 
 var posts = [{
-    id: '1',
-    title: "Rails is Omakase",
-    author: {
-        name: "d2h"
-    },
-    date: new Date('12-27-2012'),
-    excerpt: "There are lots of à la carte software environments in this world. Places where in order to eat, you must first carefully look over the menu of options to order exactly what you want.",
-    content: "I want this for my ORM, I want that for my template language, and let's finish it off with this routing library. Of course, you're going to have to know what you want, and you'll rarely have your horizon expanded if you always order the same thing, but there it is. It's a very popular way of consuming software.\n\nRails is not that. Rails is omakase."
+  id: '1',
+  title: "Rails is Omakase",
+  author: {
+    name: "d2h"
+  },
+  date: new Date('12-27-2012'),
+  excerpt: "There are lots of à la carte software environments in this world. Places where in order to eat, you must first carefully look over the menu of options to order exactly what you want.",
+  content: "I want this for my ORM, I want that for my template language, and let's finish it off with this routing library. Of course, you're going to have to know what you want, and you'll rarely have your horizon expanded if you always order the same thing, but there it is. It's a very popular way of consuming software.\n\nRails is not that. Rails is omakase."
 }, {
-    id: '2',
-    title: "The Parley Letter",
-    author: {
-        name: "d2h"
-    },
-    date: new Date('12-24-2012'),
-    excerpt: "My [appearance on the Ruby Rogues podcast](http://rubyrogues.com/056-rr-david-heinemeier-hansson/) recently came up for discussion again on the private Parley mailing list.",
-    content: "A long list of topics were raised and I took a time to ramble at large about all of them at once. Apologies for not taking the time to be more succinct, but at least each topic has a header so you can skip stuff you don't care about.\n\n### Maintainability\n\nIt's simply not true to say that I don't care about maintainability. I still work on the oldest Rails app in the world."
+  id: '2',
+  title: "The Parley Letter",
+  author: {
+    name: "d2h"
+  },
+  date: new Date('12-24-2012'),
+  excerpt: "My [appearance on the Ruby Rogues podcast](http://rubyrogues.com/056-rr-david-heinemeier-hansson/) recently came up for discussion again on the private Parley mailing list.",
+  content: "A long list of topics were raised and I took a time to ramble at large about all of them at once. Apologies for not taking the time to be more succinct, but at least each topic has a header so you can skip stuff you don't care about.\n\n### Maintainability\n\nIt's simply not true to say that I don't care about maintainability. I still work on the oldest Rails app in the world."
 }];
 
 var pages = [{
-    id: '1',
-    title: 'My Test Page',
-    content: 'Hello, this is a test page.'
+  id: '1',
+  title: 'My Test Page',
+  content: 'Hello, this is a test page.'
 }, {
-    id: '2',
-    title: 'My Second Test Page',
-    content: 'Hello, this is another test page. Lorem ipsum and stuff.'
+  id: '2',
+  title: 'My Second Test Page',
+  content: 'Hello, this is another test page. Lorem ipsum and stuff.'
 }];
 
 
 App.Router.map(function () {
-    this.resource('about');
-    this.resource('page', function () {
-        this.resource('page', {
-            path: ':page_id'
-        });
+  this.resource('about');
+  this.resource('page', function () {
+    this.resource('page', {
+      path: ':page_id'
     });
-    this.resource('posts', function () {
-        this.resource('post', {
-            path: ':post_id'
-        });
+  });
+  this.resource('posts', function () {
+    this.resource('post', {
+      path: ':post_id'
     });
+  });
+  this.resource('posts', { path: '/' });
 });
 
 
@@ -50,56 +51,63 @@ App.Router.map(function () {
  * POSTS
  */
 App.PostsRoute = Ember.Route.extend({
-    model: function () {
-        return posts;
-    }
+  model: function () {
+    return posts;
+  }
 });
 
 App.PostRoute = Ember.Route.extend({
-    model: function (params) {
-        return posts.findBy('id', params.post_id);
-    }
+  model: function (params) {
+    return posts.findBy('id', params.post_id);
+  }
 });
 
 App.PostController = Ember.ObjectController.extend({
-    isEditing: false,
+  isEditing: false,
 
-    actions: {
-        edit: function () {
-            this.set('isEditing', true);
-        },
+  actions: {
+    edit: function () {
+      this.set('isEditing', true);
+    },
 
-        doneEditing: function () {
-            this.set('isEditing', false);
-        }
+    doneEditing: function () {
+      this.set('isEditing', false);
     }
+  }
 });
 
 /* 
  * PAGES
  */
-App.PagesRoute = Ember.Route.extend({
-   model: function () {
-       return pages;
-   } 
-});
-
 App.PageRoute = Ember.Route.extend({
-   model: function (params) {
-       return pages.findBy('id', params.page_id);
-   } 
+  model: function (params) {
+    return pages.findBy('id', params.page_id);
+  }
 });
 
 App.PageController = Ember.ObjectController.extend({
-    
+  isEditing: false,
+
+  actions: {
+    edit: function () {
+      this.set('isEditing', true);
+    },
+
+    doneEditing: function () {
+      this.set('isEditing', false);
+    }
+  }
 });
 
+/*
+ * HELPERS
+ */
 var showdown = new Showdown.converter();
 
-Ember.Handlebars.helper('format-markdown', function (input){
-    return new Handlebars.SafeString(showdown.makeHtml(input));
+Ember.Handlebars.helper('format-markdown', function (input) {
+  return new Handlebars.SafeString(showdown.makeHtml(input));
 });
 
-Ember.Handlebars.helper('format-date', function (date){
-    return moment(date).fromNow();
+Ember.Handlebars.helper('format-date', function (date) {
+  return moment(date).fromNow();
 });
