e567ac1c640ae71bad73e60bc961e08e6c787763
Sebastian Herrmann <sebastian@herrherrmann.net>
2015-02-23 13:27:48 +0100

    Added new line at end of file.

--- js/models/post.js
+++ js/models/post.js
@@ -102,20 +102,3 @@ App.PostController = Ember.ObjectController.extend({
   },
   isEditing: false
 });
-
-
-App.Post.FIXTURES = [{
-  id: 1,
-  title: "Lorem Ipsum Header",
-  excerpt: "There are lots of à la carte software environments in this world. Places where in order to eat, you must first carefully look over the menu of options to order exactly what you want.",
-  body: "I want this for my ORM, I want that for my template language, and let's finish it off with this routing library. Of course, you're going to have to know what you want, and you'll rarely have your horizon expanded if you always order the same thing, but there it is. It's a very popular way of consuming software.\n\nRails is not that. Rails is omakase.",
-  date: new Date('01-02-2015'),
-  tags: "bit, chip"
-}, {
-  id: 2,
-  title: "Here's another Header!",
-  excerpt: "Here is some excerpt text, wow.",
-  body: "... and here's the legit content of this post!\n\n_Incredible_, right?",
-  date: new Date('02-13-2015'),
-  tags: "none, sense"
-}];
