fa28b051a3cdb2643eb8048fb36678f5add13083
shlansn <shlansn@outlook.de>
2015-03-12 17:10:14 +0100

    Revert "Tobi implemented live search"
    This reverts commit 79a89800195dc52bbe17eac0dd21d603bc3c9a16.

--- js/controllers/search.js
+++ js/controllers/search.js
@@ -1,35 +1,19 @@
-$(function(){
-  
-$("#searchboxding").keyup(function() {
-  var controller = App.__container__.lookup("controller:search");
-  controller.send('startSearch',this.value);
-});
-
-});
-
 App.SearchRoute = Ember.Route.extend({
   model: function () {
     //window.searchString = prompt("Please enter the string", "test");
     window.searchString = window.mySearchString;
 
     window.infoArray = new Array(0);
-    window.infoArray2 = new Array(0);
 
     return App.Post.store.filter('post', function(post) {
-      
+      /*
       var postArray = new Array(0);
       postArray.push(post.get('title'));
       postArray.push(post.get('excerpt'));
       postArray.push(post.get('body'));
       postArray.push(post.get('tags'));
-
-      var dataObject = {
-        title: post.get('title')
-      };
-
-      infoArray2.push(dataObject);
       infoArray.push(postArray);
-      
+      */
 
       if (!(post.get('title') === undefined)) {
         var tempTitle = post.get('title');
@@ -54,10 +38,7 @@ App.SearchRoute = Ember.Route.extend({
       return (false);
     });
   },
-});
-
-App.SearchController = Ember.ArrayController.extend({
-    actions: {
+  actions: {
     startSearch: function (params) { 
       window.mySearchString = params;
       this.transitionTo('posts');
@@ -300,82 +281,6 @@ App.CockpitView = Ember.View.extend({
         responsive : true
       });
 
-
-      //////////////////////////////////////////////
-      ////////////////////
-      ///////////////////////////////////////////////
-        Highcharts.setOptions({
-            global: {
-                useUTC: false
-            }
-        });
-
-      $('#container2').highcharts({
-            chart: {
-                type: 'spline',
-                animation: Highcharts.svg, // don't animate in old IE
-                marginRight: 10,
-                events: {
-                    load: function () {
-
-                        // set up the updating of the chart each second
-                        var series = this.series[0];
-                        setInterval(function () {
-                            var x = (new Date()).getTime(), // current time
-                                y = (Math.random()+1)*1000;
-                            series.addPoint([x, y], true, true);
-                        }, 1000);
-                    }
-                }
-            },
-            title: {
-                text: 'Live Traffic (Random animated Data)'
-            },
-            xAxis: {
-                type: 'datetime',
-                tickPixelInterval: 150
-            },
-            yAxis: {
-                title: {
-                    text: 'People looking at your site'
-                },
-                plotLines: [{
-                    value: 0,
-                    width: 1,
-                    color: '#808080'
-                }]
-            },
-            tooltip: {
-                formatter: function () {
-                    return '<b>' + this.series.name + '</b><br/>' +
-                        Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
-                        Highcharts.numberFormat(this.y, 2);
-                }
-            },
-            legend: {
-                enabled: false
-            },
-            exporting: {
-                enabled: false
-            },
-            series: [{
-                name: 'Random data',
-                data: (function () {
-                    // generate an array of random data
-                    var data = [],
-                        time = (new Date()).getTime(),
-                        i;
-
-                    for (i = -19; i <= 0; i += 1) {
-                        data.push({
-                            x: time + i * 1000,
-                            y: (Math.random()+1)*1000
-                        });
-                    }
-                    return data;
-                }())
-            }]
-        });
   }
 })
 
