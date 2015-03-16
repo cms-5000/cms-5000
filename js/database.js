4c02d50e1bdc90ccf821381798aea4d040449742
Sebastian Herrmann <sebastian@herrherrmann.net>
2015-03-16 21:15:05 +0100

    Added favicon. Updated ember pouch adapter (necessary for our current ember-data). Updated showdown.js library.

--- js/database.js
+++ js/database.js
@@ -2,15 +2,16 @@
 //var db  = new PouchDB('local_couch');
 //
 //function doSync() {
-//  db.sync(remote, {live: true})
-//    .on('error', function(err) {
+//  db.sync(remote, {
+//    live:  true,
+//    retry: true
+//  }).on('error', function(err) {
 //      // Retry connection every 5 seconds
 //      setTimeout(doSync, 5000);
 //    });
 //}
-//
 //doSync();
-
+//
 //App.ApplicationAdapter = EmberPouch.Adapter.extend({
 //  db: db
 //});
