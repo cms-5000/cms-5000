3d1784f43e3be0fbe2aa036cdeb96dc7ec7abaab
Tobias <fragelto@web.de>
2015-03-28 12:31:32 +0100

    Added onChange listener for Database
    Now a synch is started when a change gets detected and no user and
    password is neaded for the db

--- js/database.js
+++ js/database.js
@@ -1,14 +1,16 @@
-//var remote = new PouchDB('https://couchdb-b44c24.smileupps.com/cms');
+//var remote = new PouchDB('https://cms5000:web5000@couchdb-b44c24.smileupps.com/cms');
 //var db  = new PouchDB('local_couch');
 //
 //function doSync() {
 //  db.sync(remote, {
 //    live:  true,
 //    retry: true
+//  }).on('change', function (change) {
+//    doSync();
 //  }).on('error', function(err) {
 //      // Retry connection every 5 seconds
 //      setTimeout(doSync, 5000);
-//    });
+//  });
 //}
 //doSync();
 //
